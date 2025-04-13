fcb9181d306b0231c09b02394d96a2a8ca6a7b21
nathan-n-poon <nathannpoon@gmail.com>
2025-04-13 14:41:55 -0700

    yeah aggregation/disaggregation works
    really messy
    really messy code

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
 from curses.textpad import Textbox
 
 import matplotlib.pyplot as plt
+from matplotlib.axis import YAxis, XAxis
 from matplotlib.patches import Rectangle
+from mpl_toolkits.axes_grid1.axes_size import AxesY
 
 import gather_stats
 
@@ -25,13 +27,19 @@ def vis_chapter(chapter_stats, y_offset):
                  markeredgecolor='#e8bd79',)
         chapter_stats.occ_pos_plot_loc.append(plot_loc)
 
-def add_aggregate_label(x, y, width):
-    ax.add_patch(Rectangle((x,y-marker_height), width, 2*marker_height))
+def add_aggregate_label(x, y, width, aggregate_size):
+    # redshift the larger the aggregate
+    green_val = max(0, 1 - (0.05 * aggregate_size))
+    new_patch = Rectangle((x, y-marker_height), width, 2 * marker_height,
+                           zorder=10,
+                           color=(1, green_val, 0))
+    cur_aggregates.append(new_patch)
+    ax.add_patch(new_patch)
+    # 0.1 works on my display :P
+    cur_aggregates.append(plt.text(x + width/2 - 0.1, y - 0.1, str(aggregate_size), zorder=11))
 
-
-def aggregate_chapter_pos(chapter_stats, y_offset):
-    coalesce_width = 0.3
-
+cur_aggregates = []
+def aggregate_chapter_pos(chapter_stats, y_offset, coalesce_width):
     # cute alias
     locs = chapter_stats.occ_pos_plot_loc
     if len(locs) < 1:
@@ -49,13 +57,38 @@ def aggregate_chapter_pos(chapter_stats, y_offset):
             aggregate_size += 1
         else:
             if aggregate_size > 1:
-                add_aggregate_label(aggregate_base, y_offset, (aggregate_apex - aggregate_base))
+                add_aggregate_label(aggregate_base, y_offset, (aggregate_apex - aggregate_base), aggregate_size)
                 # ax.add_patch(Textbox())
             aggregate_base = locs[idx]
             aggregate_apex = locs[idx]
             aggregate_size = 1
     if aggregate_size > 1:
-        add_aggregate_label(aggregate_base, y_offset, (aggregate_apex - aggregate_base))
+        add_aggregate_label(aggregate_base, y_offset, (aggregate_apex - aggregate_base), aggregate_size)
+
+def xLimChanged(axes):
+    global cur_aggregates
+    global prev_x_lim
+    (bot, top) = axes.get_xlim()
+    bounds = round(top) - round(bot)
+    if bounds != prev_x_lim:
+        for aggregate in cur_aggregates:
+            aggregate.remove()
+        cur_aggregates = []
+        print("calling aggregate all with: " + str(0.3 * bounds/init_x_lim))
+        aggregate_all(0.3 * bounds/init_x_lim)
+        prev_x_lim = bounds
+def yLimChanged(axes):
+    global prev_y_lim
+    (bot, top) = axes.get_ylim()
+    bounds = round(top) - round(bot)
+    if bounds != prev_y_lim:
+        prev_y_lim = bounds
+
+def aggregate_all(coalesce_width):
+    count = 1
+    for chapter_stats in chapters_stats:
+        aggregate_chapter_pos(chapter_stats, top_margin * -count, coalesce_width)
+        count += 1
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -85,11 +118,18 @@ for chapter_stats in chapters_stats:
     vis_chapter(chapter_stats, top_margin * -count)
     count += 1
 
-count = 1
-for chapter_stats in chapters_stats:
-    aggregate_chapter_pos(chapter_stats, top_margin * -count)
-    count += 1
+aggregate_all(0.3)
+
+(a, b) = ax.get_xlim()
+init_x_lim = round(b) - round(a)
+prev_x_lim = init_x_lim
+(a, b) = ax.get_ylim()
+prev_y_lim = round(b) - round(a)
+cb_registry = ax.callbacks
+cidx = cb_registry.connect('xlim_changed', xLimChanged)
+cidy = cb_registry.connect('ylim_changed', yLimChanged)
 
 plt.axis('off')
 plt.show()
+
 
